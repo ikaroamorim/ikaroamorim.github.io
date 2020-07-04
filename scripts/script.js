72672e728e952787e8baa8ac5bbdbe68ac92861c
ikaroamorim <ikaro.amorim@gmail.com>
2020-07-04 12:48:00 -0300

    Navbar and expirience javascript actions added

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $(window).scroll(function(){
+        if($(window).scrollTop() > $(window).innerHeight()){
+            console.log("Executou")
+            $("#navbar").addClass("nav-fixed");
+        }else{
+            $("#navbar").removeClass("nav-fixed");
+        }
+        console.log($(window).scrollTop());
+    })
+
+    $("#btn-1").on("click", function(){
+        $('#btn-2').removeClass('active');
+        $('#btn-3').removeClass('active');
+        $('#btn-1').addClass('active');
+
+        $('#ctn-2').removeClass('content-active');
+        $('#ctn-3').removeClass('content-active');
+        $('#ctn-1').addClass('content-active');
+    })
+
+    $("#btn-2").on("click", function(){
+        $('#btn-1').removeClass('active');
+        $('#btn-3').removeClass('active');
+        $('#btn-2').addClass('active');
+
+        $('#ctn-1').removeClass('content-active');
+        $('#ctn-3').removeClass('content-active');
+        $('#ctn-2').addClass('content-active');
+    })
+
+    $("#btn-3").on("click", function(){
+        $('#btn-1').removeClass('active');
+        $('#btn-2').removeClass('active');
+        $('#btn-3').addClass('active');
+
+        $('#ctn-1').removeClass('content-active');
+        $('#ctn-2').removeClass('content-active');
+        $('#ctn-3').addClass('content-active');
+    })
+
+
+
+});
